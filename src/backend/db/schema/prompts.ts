93c4b4309d19ca0ee06bc2e5c65837924ce91627
probeV <ahnjaemin1208@gmail.com>
2026-03-05 17:58:57 +0900

    feat: prompts schema use object to structured

---
+++ src/backend/db/schema/prompts.ts
@@ -0,0 +1,63 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+/**
+ * Configuration to maintain post quality and persona for each subject.
+ */
+export interface WritingConfig {
+  identity: string; // AI persona
+  principles: string[]; // key values for content
+  structure: {
+    // guidelines for the each section
+    intro: string;
+    body: string;
+    outro: string;
+  };
+  forbiddenTopics: string[]; 
+  writingStyle: string[];
+}
+
+/**
+ * Configuration to maintain thumbnail consistency and branding for each subject.
+ */
+export interface ThumbnailConfig {
+  visual: {
+    // basic visualization config
+    ratio: string;
+    backgroundColor: string;
+    textColor: string;
+  };
+  iconStyle: string; // specific graphic style in thumnail
+  thumbnailGuides: string[]; // guideline in detailed
+}
+
+/**
+ * Configuration to maintain the quality and relevance of images within the post body.
+ */
+export interface InnerImageConfig {
+  visual: {
+    // basic visualization config
+    ratio: string;
+  };
+  artStyle: string; // preferred artistic style
+  innerImageGuides: string[]; // guideline in detailed
+}
+
+export const prompts = sqliteTable("Prompts", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+
+  // save object to use JSON mode
+  writingConfig: text("writing_config", { mode: "json" })
+    .$type<WritingConfig>()
+    .notNull(),
+  thumbnailConfig: text("thumbnail_config", { mode: "json" })
+    .$type<ThumbnailConfig>()
+    .notNull(),
+  innerImageConfig: text("inner_image_config", { mode: "json" })
+    .$type<InnerImageConfig>()
+    .notNull(),
+
+  createdAt: integer("created_at", { mode: "timestamp" })
+    .notNull()
+    .$defaultFn(() => new Date()),
+});
